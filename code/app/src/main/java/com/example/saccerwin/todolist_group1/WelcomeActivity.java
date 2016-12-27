c58da30c4352a198641f446423119f1fcf307e93
duyenbui <anhduyen1510@gmail.com>
2016-12-27 14:26:03 +0700

    add welcomeactivity

--- code/app/src/main/java/com/example/saccerwin/todolist_group1/WelcomeActivity.java
+++ code/app/src/main/java/com/example/saccerwin/todolist_group1/WelcomeActivity.java
@@ -4,10 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,117 +20,137 @@ import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    private ViewPager mViewPager;
-    private IntroViewPagerAdapter mPagerAdapter;
-    private LinearLayout mDotsLayout;
-    private TextView[] mDots;
-    private int[] mLayouts;
-    private Button mBtnSkip;
-    private Button mBtnNext;
-    private PrefManager mPrefManager;
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+    private PrefManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (!PrefManager.getInstance(this).isFirstTimeLaunch()) {
+        // Checking for first time launch - before calling setContentView()
+        prefManager = new PrefManager(this);
+        if (!prefManager.isFirstTimeLaunch()) {
             launchHomeScreen();
+            finish();
         }
 
-        //Making notification bar transparent
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
         setContentView(R.layout.activity_welcome);
 
-        mViewPager = (ViewPager) findViewById(R.id.view_pager);
-        mDotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
-        mBtnSkip = (Button) findViewById(R.id.btn_skip);
-        mBtnNext = (Button) findViewById(R.id.btn_next);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        btnSkip = (Button) findViewById(R.id.btn_skip);
+        btnNext = (Button) findViewById(R.id.btn_next);
 
-        /**
-         * Layouts of all welcome slides
-         * add few more layouts if you want
-         */
-        mLayouts = new int[]{R.layout.welcome_slide1, R.layout.welcome_slide2, R.layout.welcome_slide3, R.layout.welcome_slide4};
+
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = new int[]{
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2,
+                R.layout.welcome_slide3,
+                R.layout.welcome_slide4};
+
+        // adding bottom dots
         addBottomDots(0);
+
+        // making notification bar transparent
         changeStatusBarColor();
 
-        mPagerAdapter = new IntroViewPagerAdapter();
-        mViewPager.setAdapter(mPagerAdapter);
-        mViewPager.addOnPageChangeListener(mViewPagerChangeListener);
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
 
-        mBtnSkip.setOnClickListener(new View.OnClickListener(){
-
+        btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 launchHomeScreen();
             }
         });
 
-        mBtnNext.setOnClickListener(new View.OnClickListener() {
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /**
-                 * Checking for last page if last page home screen will be launched
-                 */
-                int current = getItem(1);
-                if (current < mLayouts.length) {
-                    // move to nex screen
-                    mViewPager.setCurrentItem(current);
+                // checking for last page
+                // if last page home screen will be launched
+                int current = getItem(+1);
+                if (current < layouts.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
                 } else {
                     launchHomeScreen();
                 }
-
             }
         });
     }
-    private ViewPager.OnPageChangeListener mViewPagerChangeListener = new ViewPager.OnPageChangeListener() {
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
         }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    private void launchHomeScreen() {
+        prefManager.setIsFirstTimeLaunch(false);
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+        finish();
+    }
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
 
         @Override
         public void onPageSelected(int position) {
             addBottomDots(position);
-            //Change the next button text 'NEXT'/'GOT IT'
-            if (position == mLayouts.length - 1) {
-                mBtnNext.setText(getString(R.string.start));
-                mBtnSkip.setVisibility(View.GONE);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.length - 1) {
+                // last page. make button text to GOT IT
+                btnNext.setText(getString(R.string.start));
+                btnSkip.setVisibility(View.GONE);
             } else {
-                //Still pages are left
-                mBtnNext.setText(getString(R.string.next));
-                mBtnSkip.setVisibility(View.VISIBLE);
+                // still pages are left
+                btnNext.setText(getString(R.string.next));
+                btnSkip.setVisibility(View.VISIBLE);
             }
         }
 
         @Override
-        public void onPageScrollStateChanged(int state) {
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
 
         }
     };
-
-    private void addBottomDots(int currentPage) {
-        mDots = new TextView[mLayouts.length];
-
-        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
-        int[] colorsInActive = getResources().getIntArray(R.array.array_dot_inactive);
-
-        mDotsLayout.removeAllViews();
-        for (int i = 0; i < mDots.length; i++) {
-            mDots[i] = new TextView(this);
-            mDots[i].setText(Html.fromHtml("•"));
-            mDots[i].setTextSize(35);
-            mDots[i].setTextColor(colorsInActive[currentPage]);
-            mDotsLayout.addView(mDots[i]);
-        }
-        if (mDots.length > 0) {
-            mDots[currentPage].setTextColor(colorsActive[currentPage]);
-        }
-    }
 
     /**
      * Making notification bar transparent
@@ -143,40 +163,35 @@ public class WelcomeActivity extends AppCompatActivity {
         }
     }
 
-    private int getItem(int i) {
-        return mViewPager.getCurrentItem() + i;
-    }
+    /**
+     * View pager adapter
+     */
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
 
-    private void launchHomeScreen() {
-        Intent intent = new Intent(this, NavigationDrawerMainActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    public class IntroViewPagerAdapter extends PagerAdapter {
-        private LayoutInflater mInflater;
-
-        public IntroViewPagerAdapter() {
-            super();
+        public MyViewPagerAdapter() {
         }
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View view = mInflater.inflate(mLayouts[position], container, false);
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
             container.addView(view);
+
             return view;
         }
 
         @Override
         public int getCount() {
-            return mLayouts.length;
+            return layouts.length;
         }
 
         @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
         }
+
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
---
+++ code/app/src/main/java/com/example/saccerwin/todolist_group1/objects/Group.java
@@ -0,0 +1,46 @@
+package com.example.saccerwin.todolist_group1.objects;
+
+
+/**
+ * Created by duyenbui on 08/12/2016.
+ */
+
+public class Group {
+    private String name;
+    private String decrips;
+    private String image;
+    private int countTask;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDecrips() {
+        return decrips;
+    }
+
+    public void setDecrips(String decrips) {
+        this.decrips = decrips;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getCountTask() {
+        return countTask;
+    }
+
+    public void setCountTask(int countTask) {
+        this.countTask = countTask;
+    }
+}
