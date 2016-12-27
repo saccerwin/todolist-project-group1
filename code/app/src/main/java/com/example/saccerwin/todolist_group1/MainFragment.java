1b4b022b984beb1dbbb709f1b07b24d8826961e3
duyle307 <ngocduy307@gmail.com>
2016-12-27 15:42:30 +0700

    Update Main todo list

--- code/app/src/main/java/com/example/saccerwin/todolist_group1/MainFragment.java
+++ code/app/src/main/java/com/example/saccerwin/todolist_group1/MainFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
@@ -100,6 +102,15 @@ public class MainFragment extends Fragment {
         tvDay = (TextView) view.findViewById(R.id.tvDay);
         tvMonthYear = (TextView) view.findViewById(R.id.tvMonthYear);
         recyclerTask = (RecyclerView) view.findViewById(R.id.recyclerTask);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fabAdd);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         setDayMonthYear();
 
